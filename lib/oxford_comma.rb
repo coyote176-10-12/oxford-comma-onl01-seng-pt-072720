6a35a9005146beb259fcb4647e8d57bdfbd4840c
Javier Aguilar <coyote176@msn.com>
2020-08-03 23:36:54 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,3 +2,14 @@ def oxford_comma(array)
 array["kiwi"].join
  return array
 end
+def oxford_comma(array)
+
+end 
+  if array.length == 1
+    return array[0]
+  elsif array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  else
+    return "#{array[0..-2].join(", ")}, and #{array.last}"
+  end
+end
