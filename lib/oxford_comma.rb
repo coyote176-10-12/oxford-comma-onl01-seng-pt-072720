7384abced2330fa124ef19bc5d467d4619808ba3
Javier Aguilar <coyote176@msn.com>
2020-08-04 00:01:03 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,11 @@
 def oxford_comma(array)
- array = ["kiki", "durian"].join
- return array
+
+if array.length < 2
+    array.join
+  elsif array.length == 2
+    array.insert(1, "and").join(" ")
+  else
+    str = ", and " + array.pop
+    str = array.join(", ") << str
+  end
 end 
- 
